8a79003b62934bb09dd43b3dca5515ff3397f594
jakub.prochazka.023 <jakub.prochazka.023@PC2022-19>
2025-04-22 13:31:04 +0200

    22

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 
-export const App =() => (
-    <p>Hai</p>
-);
 export const Epp =() =>(
     <h1>world</h1>
 );
 export const did = () =>(
     <div>hello</div>
 );
+const arrow1 = (x:number):number => x + 1
+
+const arrow2 = (x:number):number => (
+  x + 1
+)
+
+const arrow3 = (x:number):number => { x = 2; return x + 1}
+
+const arrow4 = (x:number):number => {
+   x = 2; 
+   return (
+    x + 1
+   )
+}
+
+
+const App = () => {
+
+  return (
+    <div>
+      <p>Ahoj nigger</p>
+    </div>
+  )
+}
+
+export default App
